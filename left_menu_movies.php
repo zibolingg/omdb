20f33b0b5c2e22103d68eb8913665b551a9501b5
Gada23 <79663475+Gada23@users.noreply.github.com>
2021-06-14 19:38:36 -0500

    Update left_menu_movies.php

--- left_menu_movies.php
+++ left_menu_movies.php
@@ -14,7 +14,21 @@
   	<img src="./images/data.png">
   	<br/>Data<br/></div>
   </a>
-
+  
+  <a href = "movies_anagram.php">
+  	<div <?php if($left_selected == "ANAGRAM")
+  	{ echo 'class="menu-left-current-page"'; } ?>>
+    <img src="">
+  	<br/>anagram<br/></div>
+  </a>
+  
+    <a href = "movies_keyword.php">
+  	<div <?php if($left_selected == "KEYWORD")
+  	{ echo 'class="menu-left-current-page"'; } ?>>
+  	<img src="">
+  	<br/>Keyword<br/></div>
+  </a>
+  
   <a href = "movies_media.php">
   	<div <?php if($left_selected == "MEDIA")
   	{ echo 'class="menu-left-current-page"'; } ?>>
@@ -22,19 +36,25 @@
   	<br/>MEDIA<br/></div>
   </a>
 
-  <a href = "movies_songs.php">
-    <div <?php if($left_selected == "SONGS")
+  <a href = "movies_numbers.php">
+    <div <?php if($left_selected == "NUMBERS")
     { echo 'class="menu-left-current-page"'; } ?>>
-    <img src="./images/songs.png">
-    <br/>Songs<br/></div>
+    <img src="">
+    <br/>Number<br/></div>
   </a>
 
-  <a href = "movies_people.php">
+  <a href = "movies_quotes.php">
   	<div <?php if($left_selected == "PEOPLE")
   	{ echo 'class="menu-left-current-page"'; } ?>>
-  	<img src="./images/people.png">
-  	<br/>People<br/></div>
+  	<img src="">
+  	<br/>Quotes<br/></div>
   </a>
 
+  <a href = "movies_trivia.php">
+  	<div <?php if($left_selected == "TRIVIA")
+  	{ echo 'class="menu-left-current-page"'; } ?>>
+  	<img src="">
+  	<br/>Trivia<br/></div>
+  </a>
 
 </div>
