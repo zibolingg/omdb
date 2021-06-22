5e074a1f4f4b67a00ddaf1eb743b11f015214218
Gada23 <79663475+Gada23@users.noreply.github.com>
2021-06-21 19:14:17 -0500

    Add files via upload

---
+++ attach_song.php
@@ -0,0 +1,18 @@
+<?php
+    require 'db_configuration.php';
+    $movie_id = $_POST['movie_id'];
+    $total_elements = $_POST['total_elements'];
+    for($i=1 ; $i<$total_elements ; $i++){
+      if(!empty($_POST['song_id'.$i.''])){
+      $song_id[$i] = $_POST['song_id'.$i.''];
+      $query = "INSERT INTO `movie_song`(`id`, `movie_id`, `song_id`) VALUES ('','$movie_id','$song_id[$i]')";
+    $result = mysqli_query($db,$query);
+    }
+    else{
+      // break;
+    }
+
+    }
+     header("location:add_song.php?movie_id=$movie_id");
+
+?>
