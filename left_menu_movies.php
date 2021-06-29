499985ee8bb52f7d4ae30d2dbfc19ac4c429ca65
Gada23 <79663475+Gada23@users.noreply.github.com>
2021-06-28 20:33:50 -0500

    Update left_menu_movies.php

--- left_menu_movies.php
+++ left_menu_movies.php
@@ -14,47 +14,48 @@
   	<img src="./images/data.png">
   	<br/>Data<br/></div>
   </a>
-  
+
   <a href = "movies_anagram.php">
-  	<div <?php if($left_selected == "ANAGRAM")
-  	{ echo 'class="menu-left-current-page"'; } ?>>
-    <img src="">
-  	<br/>anagram<br/></div>
+    <div <?php if($left_selected == "ANAGRAM")
+    { echo 'class="menu-left-current-page"'; } ?>>
+    <img src="./images/anagram.png">
+    <br/>Anagram<br/></div>
   </a>
-  
-    <a href = "movies_keyword.php">
-  	<div <?php if($left_selected == "KEYWORD")
-  	{ echo 'class="menu-left-current-page"'; } ?>>
-  	<img src="">
-  	<br/>Keyword<br/></div>
+
+  <a href = "movies_keywords.php">
+    <div <?php if($left_selected == "KEYWORD")
+    { echo 'class="menu-left-current-page"'; } ?>>
+    <img src="./images/keyword.png">
+    <br/>Keyword<br/></div>
   </a>
-  
+
   <a href = "movies_media.php">
-  	<div <?php if($left_selected == "MEDIA")
-  	{ echo 'class="menu-left-current-page"'; } ?>>
-  	<img src="./images/media.png">
-  	<br/>MEDIA<br/></div>
+    <div <?php if($left_selected == "MEDIA")
+    { echo 'class="menu-left-current-page"'; } ?>>
+    <img src="./images/media.png">
+    <br/>MEDIA<br/></div>
   </a>
 
   <a href = "movies_numbers.php">
     <div <?php if($left_selected == "NUMBERS")
     { echo 'class="menu-left-current-page"'; } ?>>
-    <img src="">
-    <br/>Number<br/></div>
+    <img src="./images/number.png">
+    <br/>Numbers<br/></div>
   </a>
 
   <a href = "movies_quotes.php">
-  	<div <?php if($left_selected == "PEOPLE")
-  	{ echo 'class="menu-left-current-page"'; } ?>>
-  	<img src="">
-  	<br/>Quotes<br/></div>
+    <div <?php if($left_selected == "QUOTES")
+    { echo 'class="menu-left-current-page"'; } ?>>
+    <img src="./images/quote.png">
+    <br/>Quotes<br/></div>
   </a>
 
   <a href = "movies_trivia.php">
-  	<div <?php if($left_selected == "TRIVIA")
-  	{ echo 'class="menu-left-current-page"'; } ?>>
-  	<img src="">
-  	<br/>Trivia<br/></div>
+    <div <?php if($left_selected == "TRIVIA")
+    { echo 'class="menu-left-current-page"'; } ?>>
+    <img src="./images/trivia.png">
+    <br/>Trivia<br/></div>
   </a>
+
 
 </div>
